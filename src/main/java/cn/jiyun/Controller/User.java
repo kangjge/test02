bac4e296669c5087d357090753cdeab81876016f
kangjge <1760262749@qq.com>
2020-04-22 11:41:49 +0800

    八荣八耻！！

---
+++ src/main/java/cn/jiyun/Controller/User.java
@@ -0,0 +1,14 @@
+package cn.jiyun.Controller;
+
+public class User {
+
+    public  String get1(){
+
+        return "以团结互爱为荣";
+    }
+
+    public  String get2(){
+
+        return "以按时出勤为荣";
+    }
+}
